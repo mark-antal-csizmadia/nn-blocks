756c5b80c7eb3903a827281918c18f04bb72e554
mark-antal-csizmadia <mark.antal.csizmadia@gmail.com>
2021-04-15 10:25:29 +0200

    refactoring code into modules and keeping just the highest abtraction stuff in notebooks

---
+++ activations.py
@@ -0,0 +1,319 @@
+import numpy as np
+from copy import deepcopy
+
+
+class Activation():
+    """
+    Activation parent class.
+
+
+    Attributes
+    ----------
+    cache : dict
+        Run-time cache of attibutes such as gradients.
+
+    Methods
+    -------
+    __init__()
+        Constuctor.
+    """
+
+    def __init__(self, ):
+        self.cache = {}
+
+
+class LinearActivation(Activation):
+    """
+    Linear activation.
+    Usually followed by CategoricalHingeLoss.
+    Inherits everything from class Activation.
+
+    Attributes
+    ----------
+    cache : dict
+        Run-time cache of attibutes such as gradients.
+
+    Methods
+    -------
+    __init__()
+        Constuctor.
+    forward(z)
+        Activates the linear transformation of the layer, and
+        forward propagates activation. Activation is linear.
+    backward(g)
+        Backpropagates incoming gradient into the layer, based on the linear activation.
+    """
+
+    def __init__(self, ):
+        super().__init__()
+
+    def forward(self, z):
+        """ Activates the linear transformation of the layer, and
+        forward propagates activation. Activation is linear.
+
+        Parameters
+        ----------
+        z : numpy.ndarray
+            Linear transformation of layer.
+            Shape is unknown here, but will usually be
+            (batch size, this layer output dim = next layer input dim)
+
+        Returns
+        -------
+        numpy.ndarray
+            Linear activation.
+
+        Notes
+        -----
+        None
+        """
+        return deepcopy(z)
+
+    def backward(self, g):
+        """ Backpropagates incoming gradient into the layer, based on the linear activation.
+
+        Parameters
+        ----------
+        g : numpy.ndarray
+            Incoming gradient to the activation.
+            Shape is unknown here, but will usually be
+            (batch size, this layer output dim = next layer input dim)
+
+        Returns
+        -------
+        numpy.ndarray
+            Gradient of activation.
+            Shape is unknown here, but will usually be
+            (batch size, this layer output dim = next layer input dim)
+
+        Notes
+        -----
+        None
+        """
+        return deepcopy(g)
+
+    def __repr__(self):
+        """ Representation of class.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        repr_str : str
+            The string representation of the class.
+
+        Notes
+        -----
+        None
+        """
+        repr_str = "linear"
+        return repr_str
+
+
+class ReLUActivation(Activation):
+    """
+    ReLU activation.
+    Can be followed by virtually anything.
+    Inherits everything from class Activation.
+
+    Attributes
+    ----------
+    cache : dict
+        Run-time cache of attibutes such as gradients.
+
+    Methods
+    -------
+    __init__()
+        Constuctor.
+    forward(z)
+        Activates the linear transformation of the layer, and
+        forward propagates activation. Activation is rectified linear.
+    backward(g)
+        Backpropagates incoming gradient into the layer, based on the rectified linear activation.
+    """
+
+    def __init__(self, ):
+        super().__init__()
+
+    def forward(self, z):
+        """ Activates the linear transformation of the layer, and
+        forward propagates activation. Activation is rectified linear.
+
+        Parameters
+        ----------
+        z : numpy.ndarray
+            Linear transformation of layer.
+            Shape is unknown here, but will usually be
+            (batch size, this layer output dim = next layer input dim)
+
+        Returns
+        -------
+        numpy.ndarray
+            ReLU activation.
+
+        Notes
+        -----
+        None
+        """
+        a = np.maximum(0, z)
+        self.cache["a"] = deepcopy(a)
+        return a
+
+    def backward(self, g_in):
+        """ Backpropagates incoming gradient into the layer, based on the rectified linear activation.
+
+        Parameters
+        ----------
+        g_in : numpy.ndarray
+            Incoming gradient to the activation.
+            Shape is unknown here, but will usually be
+            (batch size, this layer output dim = next layer input dim)
+
+        Returns
+        -------
+        numpy.ndarray
+            Gradient of activation.
+            Shape is unknown here, but will usually be
+            (batch size, this layer output dim = next layer input dim)
+
+        Notes
+        -----
+        None
+        """
+        a = self.cache["a"]
+        g_out = deepcopy(g_in)
+        g_out[a <= 0] = 0.0
+        return g_out
+
+    def __repr__(self):
+        """ Representation of class.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        repr_str : str
+            The string representation of the class.
+
+        Notes
+        -----
+        None
+        """
+        repr_str = "relu"
+        return repr_str
+
+
+class SoftmaxActivation(Activation):
+    """
+    Softmax activation.
+    Usually activation of last layer and forward propagates into a CategoricalCrossEntropyLoss.
+    Inherits everything from class Activation.
+
+    Attributes
+    ----------
+    cache : dict
+        Run-time cache of attibutes such as gradients.
+
+    Methods
+    -------
+    __init__()
+        Constuctor.
+    forward(z)
+        Activates the linear transformation of the layer, and
+        forward propagates activation. Activation is softmax.
+    backward(g)
+        Backpropagates incoming gradient into the layer, based on the softmax activation.
+    """
+
+    def __init__(self, ):
+        super().__init__()
+
+    def forward(self, z):
+        """ Activates the linear transformation of the layer, and
+        forward propagates activation. Activation is softmax.
+
+        Parameters
+        ----------
+        z : numpy.ndarray
+            Linear transformation of layer.
+            Shape is unknown here, but will usually be
+            (batch size, this layer output dim = number of classes)
+
+        Returns
+        -------
+        numpy.ndarray
+            Softmax activation.
+            Shape is (batch size, this layer output dim = number of classes)
+
+        Notes
+        -----
+        None
+        """
+        # avoid numeric instability
+        z -= np.max(z, axis=1, keepdims=True)
+
+        # get unnormalized probabilities
+        # exp_scores.shape = (batch_size, K)
+        exp_z = np.exp(z)
+
+        # normalize them for each example
+        # probs.shape = (batch_size, K)
+        a = exp_z / np.sum(exp_z, axis=1, keepdims=True)
+
+        self.cache["a"] = deepcopy(a)
+
+        return a
+
+    def backward(self, g_in):
+        """ Backpropagates incoming gradient into the layer, based on the softmax activation.
+
+        Parameters
+        ----------
+        g_in : numpy.ndarray
+            Incoming gradient to the activation.
+            Shape is unknown here, but will usually be
+            (batch size, )
+
+        Returns
+        -------
+        numpy.ndarray
+            Gradient of activation.
+            Shape is unknown here, but will usually be
+            (batch size, )
+
+        Notes
+        -----
+        None
+        """
+        # g_in is y, y.shape = (batch_size,)
+        # g_out.shape = (batch_size, K)
+        n = g_in.shape[0]
+        a = self.cache["a"]
+        g_out = deepcopy(a)
+        g_out[range(n), g_in] -= 1
+        g_out /= n
+
+        return g_out
+
+    def __repr__(self):
+        """ Representation of class.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        repr_str : str
+            The string representation of the class.
+
+        Notes
+        -----
+        None
+        """
+        repr_str = "softmax"
+        return repr_str
---
+++ losses.py
@@ -0,0 +1,231 @@
+import numpy as np
+from copy import deepcopy
+
+
+class Loss():
+    """
+    Loss parent class.
+
+    ...
+
+    Attributes
+    ----------
+    cache : dict
+        Run-time cache of attributes such as gradients.
+
+    Methods
+    -------
+    __init__()
+        Constuctor.
+    """
+
+    def __init__(self, ):
+        """ Constructor.
+
+        Parameters
+        ----------
+        None
+
+
+        Notes
+        -----
+        None
+        """
+        self.cache = {}
+
+
+class CategoricalHingeLoss(Loss):
+    """
+    Categorical Hinge loss for realizing an SVM classifier.
+    Usually preceeded by a linear activation.
+    For multi-class classification.
+    Inherits everything from class Loss.
+
+    ...
+
+    Attributes
+    ----------
+    cache : dict
+        Run-time cache of attibutes such as gradients.
+
+    Methods
+    -------
+    __init__()
+        Constuctor.
+    compute_loss(scores, y, layers_reg_loss)
+        Computes loss of classifier - also includes the regularization losses from previous layers.
+    grad()
+        Computes the gradient of the loss function.
+    """
+
+    def __init__(self, ):
+        """ Constructor.
+
+        Parameters
+        ----------
+        None
+
+
+        Notes
+        -----
+        None
+        """
+        super().__init__()
+
+    def compute_loss(self, scores, y, layers_reg_loss):
+        """ Computes loss of classifier - also includes the regularization losses from previous layers.
+
+        Parameters
+        ----------
+        scores : numpy.ndarray
+            Scores. Usually from linear activation.
+            Shape is (number of data points, number of classes)
+        y : numpy.ndarray
+            True labels.
+            Shape is (number of data points, )
+        layers_reg_loss : float
+            The regularization loss from previous layers to be included
+            in the overall loss.
+
+        Returns
+        -------
+        loss : float
+            The overall loss of the classifier.
+
+        Notes
+        -----
+        None
+        """
+        # number of classes
+        c = scores.shape[1]
+        # batch size
+        n = y.shape[0]
+
+        # correct_class_scores.shape = (batch size, 1)
+        correct_class_scores = scores[range(n), y].reshape(n, 1)
+        # margin.shape = (batch size, number of classes)
+        margin = np.maximum(0, scores - correct_class_scores + 1)
+        # do not consider correct class in loss
+        margin[range(n), y] = 0
+        data_loss = margin.sum() / n
+
+        loss = data_loss + layers_reg_loss
+
+        margin[margin > 0] = 1
+        # valid_margin_count.shape = (batch size, )
+        valid_margin_count = margin.sum(axis=1)
+        # Subtract in correct class (-s_y)
+        # margin.shape = (batch size, number of classes)
+        margin[range(n), y] -= valid_margin_count
+        margin /= n
+        self.cache["g"] = deepcopy(margin)
+
+        return loss
+
+    def grad(self, ):
+        """ Computes the gradient of the loss function.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        numpy.ndarray or None
+            None if gradient has not yet been computed.
+            Shape of gradient is (batch size, number of classes)
+
+        Notes
+        -----
+        None
+        """
+        if "g" in self.cache.keys():
+            return deepcopy(self.cache["g"])
+        else:
+            return None
+
+
+class CategoricalCrossEntropyLoss(Loss):
+    """
+    Categorical cross-entropy loss.
+    Usually preceeded by a linear activation.
+    For multi-class classification.
+    Inherits everything from class Loss.
+
+    ...
+
+    Attributes
+    ----------
+    cache : dict
+        Run-time cache of attibutes such as gradients.
+
+    Methods
+    -------
+    __init__()
+        Constuctor.
+    compute_loss(scores, y, layers_reg_loss)
+        Computes loss of classifier - also includes the regularization losses from previous layers.
+    grad()
+        Computes the gradient of the loss function.
+    """
+
+    def __init__(self, ):
+        super().__init__()
+
+    def compute_loss(self, scores, y, layers_reg_loss):
+        """ Computes loss of classifier - also includes the regularization losses from previous layers.
+
+        Parameters
+        ----------
+        scores : numpy.ndarray
+            Scores. Usually from softmax activation.
+            Shape is (batch size, number of classes)
+        y : numpy.ndarray
+            True labels.
+            Shape is (batch size, )
+        layers_reg_loss : float
+            The regularization loss from previous layers to be included
+            in the overall loss.
+
+        Returns
+        -------
+        loss : float
+            The overall loss of the classifier.
+
+        Notes
+        -----
+        None
+        """
+        self.cache["g"] = deepcopy(y)
+        n = y.shape[0]
+
+        # correct_logprobs.shape = (batch_size, )
+        correct_logprobs = -np.log(scores[range(n), y])
+
+        # compute the loss: average cross-entropy loss and regularization
+        data_loss = np.sum(correct_logprobs) / n
+        loss = data_loss + layers_reg_loss
+
+        return loss
+
+    def grad(self, ):
+        """ Computes the gradient of the loss function.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        numpy.ndarray or None
+            None if gradient has not yet been computed.
+            Shape of gradient is (batch size, ). Note that the grad here is just y.
+
+        Notes
+        -----
+        None
+        """
+        if "g" in self.cache.keys():
+            return deepcopy(self.cache["g"])
+        else:
+            return None
