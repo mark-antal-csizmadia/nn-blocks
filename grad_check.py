96653765e124ac9dcc1a7018c6a51db486cb76e8
mark-antal-csizmadia <mark.antal.csizmadia@gmail.com>
2021-04-21 21:51:32 +0200

    activations better backward check done, but softmax seems to be weird (nevertheless full model grad works, so probably just something small in the activation test

--- grad_check.py
+++ grad_check.py
@@ -12,9 +12,9 @@ def loss_func(model, loss, x, y, **param_dict):
     # np matrix
     param_val = deepcopy(param_dict["param_val"])
 
-    trainable_weights = model.get_trainable_weights()
-    trainable_weights[layer_idx][param_str] = deepcopy(param_val)
-    model.set_trainable_weights(trainable_weights)
+    trainable_params = model.get_trainable_params()
+    trainable_params[layer_idx][param_str] = deepcopy(param_val)
+    model.set_trainable_params(trainable_params)
 
     scores = model.forward(x)
     #layers_reg_loss = model.get_reg_loss()
@@ -87,12 +87,12 @@ def grad_check_without_reg(model, loss, x, y, verbose, seed=None):
     print(f"starting grad check with {x.shape[0]} data points \n")
     layer_n = len(model.layers)
 
-    for layer_idx, trainable_weights_in_layer in enumerate(model.get_trainable_weights()):
+    for layer_idx, trainable_params_in_layer in enumerate(model.get_trainable_params()):
 
-        for param_str, param_val in trainable_weights_in_layer.items():
+        for param_str, param_val in trainable_params_in_layer.items():
             model_new = deepcopy(model)
 
-            trainable_weights = model_new.get_trainable_weights()
+            trainable_params = model_new.get_trainable_params()
 
             np.random.seed(seed)
             new_param_val = np.random.normal(loc=0, scale=0.01, size=param_val.shape)
@@ -103,8 +103,8 @@ def grad_check_without_reg(model, loss, x, y, verbose, seed=None):
                 "param_val": new_param_val
             }
 
-            trainable_weights[layer_idx][param_str] = deepcopy(new_param_val)
-            model_new.set_trainable_weights(trainable_weights)
+            trainable_params[layer_idx][param_str] = deepcopy(new_param_val)
+            model_new.set_trainable_params(trainable_params)
 
             print(f"--layer: {layer_idx + 1}/{layer_n}, "
                   f"{param_str}.shape={param_val.shape} ({param_val.size} params)")
@@ -123,7 +123,7 @@ def grad_check_without_reg(model, loss, x, y, verbose, seed=None):
             grad_analytic = deepcopy(grads_analytic[layer_idx]["d" + param_str])
 
             rel_error = np.abs(grad_analytic - grad_numerical) \
-                        / (np.maximum(np.abs(grad_analytic), np.abs(grad_numerical)) + 10e-20)
+                / (np.maximum(np.abs(grad_analytic), np.abs(grad_numerical)) + 10e-20)
 
             decimal = 6
             np.testing.assert_array_almost_equal(grad_numerical, grad_analytic, decimal=decimal)
@@ -132,3 +132,60 @@ def grad_check_without_reg(model, loss, x, y, verbose, seed=None):
             print(f"passed\n")
 
     print(f"completed grad check\n")
+
+
+def eval_numerical_gradient(f, x, verbose=True, h=0.00001):
+    """
+    a naive implementation of numerical gradient of f at x
+    - f should be a function that takes a single argument
+    - x is the point (numpy array) to evaluate the gradient at
+
+    From: https://cs231n.github.io/assignments2021/assignment2/
+    """
+
+    fx = f(x)  # evaluate function value at original point
+    grad = np.zeros_like(x)
+    # iterate over all indexes in x
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+
+        # evaluate function at x+h
+        ix = it.multi_index
+        oldval = x[ix]
+        x[ix] = oldval + h  # increment by h
+        fxph = f(x)  # evalute f(x + h)
+        x[ix] = oldval - h
+        fxmh = f(x)  # evaluate f(x - h)
+        x[ix] = oldval  # restore
+
+        # compute the partial derivative with centered formula
+        grad[ix] = (fxph - fxmh) / (2 * h)  # the slope
+        if verbose:
+            print(ix, grad[ix])
+        it.iternext()  # step to next dimension
+
+    return grad
+
+
+def eval_numerical_gradient_array(f, x, df, h=1e-5):
+    """
+    Evaluate a numeric gradient for a function that accepts a numpy
+    array and returns a numpy array.
+
+    From: https://cs231n.github.io/assignments2021/assignment2/
+    """
+    grad = np.zeros_like(x)
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        ix = it.multi_index
+
+        oldval = x[ix]
+        x[ix] = oldval + h
+        pos = f(x).copy()
+        x[ix] = oldval - h
+        neg = f(x).copy()
+        x[ix] = oldval
+
+        grad[ix] = np.sum((pos - neg) * df) / (2 * h)
+        it.iternext()
+    return grad
--- test_activations.py
+++ test_activations.py
@@ -1,79 +1,84 @@
 import numpy as np
 from copy import deepcopy
 from activations import SoftmaxActivation, LinearActivation, ReLUActivation
+from losses import CategoricalCrossEntropyLoss
+from grad_check import eval_numerical_gradient_array, eval_numerical_gradient
 
 
-def test_linear_activation():
+def test_linear_activation(seed=np.random.randint(low=1, high=300)):
     size = (5, 3)
-    data = np.random.normal(loc=0, scale=1, size=size)
+    np.random.seed(seed)
+    x = np.random.normal(loc=0, scale=1, size=size)
+    a_true = deepcopy(x)
 
     linear_activation = LinearActivation()
-    fw = linear_activation.forward(data)
-    np.testing.assert_array_equal(data, fw)
+    a = linear_activation.forward(x)
+    np.testing.assert_array_equal(a, a_true)
 
     size = (5, 3)
-    data = np.random.normal(loc=0, scale=1, size=size)
-    bw = linear_activation.backward(data)
-    np.testing.assert_array_equal(data, bw)
+    np.random.seed(seed + 1)
+    g_in = np.random.normal(loc=0, scale=1, size=size)
+
+    g_out = linear_activation.backward(g_in)
+    g_out_num = eval_numerical_gradient_array(linear_activation.forward, x, g_in)
+    np.testing.assert_array_almost_equal(g_out, g_out_num, decimal=9)
 
     print("test_linear_activation passed")
 
 
 def test_relu_activation():
-    size = (5, 3)
-    data = np.random.normal(loc=0, scale=1, size=size)
+    """ Test cases from: https://cs231n.github.io/assignments2021/assignment2/ """
+    x = np.linspace(-0.5, 0.5, num=12).reshape(3, 4)
+    a_true = np.array([[0., 0., 0., 0., ],
+                       [0., 0., 0.04545455, 0.13636364, ],
+                       [0.22727273, 0.31818182, 0.40909091, 0.5, ]])
 
     relu_activation = ReLUActivation()
-    fw = relu_activation.forward(data)
-    fw_true = np.maximum(0, data)
+    a = relu_activation.forward(x)
+    np.testing.assert_array_almost_equal(a, a_true, decimal=8)
 
-    size = (5, 3)
-    data = np.random.normal(loc=0, scale=1, size=size)
-
-    bw = relu_activation.backward(data)
-    bw_true = np.copy(data)
-    bw_true[fw_true <= 0] = 0.0
-
-    np.testing.assert_array_equal(fw, fw_true)
-    np.testing.assert_array_equal(bw, bw_true)
+    relu_activation = ReLUActivation()
+    np.random.seed(231)
+    x = np.random.randn(10, 10)
+    g_in = np.random.randn(*x.shape)
+    g_out_num = eval_numerical_gradient_array(relu_activation.forward, x, g_in)
+    g_out = relu_activation.backward(g_in)
+    np.testing.assert_array_almost_equal(g_out, g_out_num, decimal=9)
 
     print("test_relu_activation passed")
 
 
 def test_softmax_activation():
+    """ Test cases from: https://cs231n.github.io/assignments2021/assignment2/ """
+
+    def func(x):
+        softmax_activation = SoftmaxActivation()
+        a = softmax_activation.forward(x)
+        categoical_cross_entropy_loss = CategoricalCrossEntropyLoss()
+        loss = categoical_cross_entropy_loss.compute_loss(a, y)
+        return loss
+
     # from: https://cs231n.github.io/linear-classify/
-    z = np.array([[-2.85, 0.86, 0.28]])
+    x = np.array([[-2.85, 0.86, 0.28]])
     softmax_activation = SoftmaxActivation()
-    fw = softmax_activation.forward(z)
-    fw_true = np.array([[0.01544932, 0.63116335, 0.35338733]])
-    np.testing.assert_almost_equal(fw, fw_true)
+    a = softmax_activation.forward(x)
+    a_true = np.array([[0.01544932, 0.63116335, 0.35338733]])
+    np.testing.assert_almost_equal(a, a_true, decimal=7)
 
-    # automated testing
-    labels = 3
-    batch_size = 5
-    size = (batch_size, labels)
-    z = np.random.normal(loc=0, scale=1, size=size)
+    np.random.seed(231)
+    num_classes, num_inputs = 10, 50
+    x = 0.001 * np.random.randn(num_inputs, num_classes)
+    y = np.random.randint(num_classes, size=num_inputs)
 
     softmax_activation = SoftmaxActivation()
-    fw = softmax_activation.forward(z)
+    a = softmax_activation.forward(x)
+    categoical_cross_entropy_loss = CategoricalCrossEntropyLoss()
+    loss = categoical_cross_entropy_loss.compute_loss(a, y)
+    loss_grad = categoical_cross_entropy_loss.grad()
+    g_out = softmax_activation.backward(loss_grad)
+    g_out_num = eval_numerical_gradient(func, x, h=5e-6, verbose=False)
 
-    z -= np.max(z, axis=1, keepdims=True)
-    exp_z = np.exp(z)
-    fw_true = exp_z / np.sum(exp_z, axis=1, keepdims=True)
-
-    size = (batch_size,)
-    g_in = np.random.randint(low=0, high=labels, size=size)
-
-    bw = softmax_activation.backward(g_in)
-
-    n = g_in.shape[0]
-    bw_true = deepcopy(fw_true)
-    bw_true[range(n), g_in] -= 1
-    bw_true /= n
-
-    assert fw.shape == (batch_size, 3)
-    assert bw.shape == (batch_size, 3)
-    np.testing.assert_array_equal(fw, fw_true)
-    np.testing.assert_array_equal(bw, bw_true)
+    np.testing.assert_array_almost_equal(g_out, g_out_num, decimal=2)
 
     print("test_softmax_activation passed")
+
