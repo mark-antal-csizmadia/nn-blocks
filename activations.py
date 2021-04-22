fixed grad check related stuff for softmax, dense + bn + dense layer, and model. closes #33, closes #32, closes #31, closes #25, closes #16, closes #13, closes #34.

activations.py
@@ -285,11 +285,11 @@ class SoftmaxActivation(Activation):
         None
         """
         # avoid numeric instability
-        z -= np.max(z, axis=1, keepdims=True)
+        z_stable = z - np.max(z, axis=1, keepdims=True)
 
         # get unnormalized probabilities
         # exp_scores.shape = (batch_size, K)
-        exp_z = np.exp(z)
+        exp_z = np.exp(z_stable)
 
         # normalize them for each example
         # probs.shape = (batch_size, K)

test_activations.py
@@ -80,7 +80,7 @@ def test_softmax_activation():
     g_out = softmax_activation.backward(loss_grad)
     g_out_num = eval_numerical_gradient(func, x, h=5e-6, verbose=False)
 
-    np.testing.assert_array_almost_equal(g_out, g_out_num, decimal=2)
+    np.testing.assert_array_almost_equal(g_out, g_out_num, decimal=10)
 
     print("test_softmax_activation passed")
 