ae8e8cbd716b3664734106121d993e2383afb691
mark-antal-csizmadia <mark.antal.csizmadia@gmail.com>
2021-07-16 23:25:54 +0200

    fixed 1/4 nb. created viz_utils.py. changed the name of main.ipynb to one-layer.ipynb

---
+++ viz_utils.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+
+
+def plot_losses(history, path_save=None):
+    plt.plot(history["loss_train"], label="train")
+    plt.plot(history["loss_val"], label="val")
+    plt.grid()
+    plt.title("Loss vs. epochs")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.legend()
+    if path_save is not None:
+        plt.savefig(path_save)
+    plt.show()
+
+
+def plot_costs(history, path_save=None):
+    plt.plot(history["cost_train"], label="train")
+    plt.plot(history["cost_val"], label="val")
+    plt.grid()
+    plt.title("Cost vs. epochs")
+    plt.xlabel("Epochs")
+    plt.ylabel("Cost")
+    plt.legend()
+    if path_save is not None:
+        plt.savefig(path_save)
+    plt.show()
+
+
+def plot_accuracies(history, path_save=None):
+    plt.plot(history["accuracy_train"], label="train")
+    plt.plot(history["accuracy_val"], label="val")
+    plt.grid()
+    plt.title("Accuracy vs. epochs")
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    if path_save is not None:
+        plt.savefig(path_save)
+    plt.show()
+
+
+def plot_lrs(history, path_save=None):
+    plt.plot(history["lr"], label="lr")
+    plt.grid()
+    plt.title("Learning rate vs. epochs")
+    plt.xlabel("Epochs")
+    plt.ylabel("Learning rate")
+    plt.legend()
+    if path_save is not None:
+        plt.savefig(path_save)
+    plt.show()
